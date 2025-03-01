745b8a7b3af6618a3b49d30659e7b20982d64712
diazoangga <diazangga.ep@gmail.com>
2025-03-01 15:51:57 +0000

    fix issues with DatasetLoader

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,216 +1,163 @@
-
-from genericpath import exists
-from glob import glob
-from sklearn.model_selection import train_test_split
-from PIL import Image
-
-import os
-import random
 import tensorflow as tf
-import numpy as np
-import cv2
+import os
+from sklearn.model_selection import train_test_split
+from genericpath import exists
+
+TRAIN_VAL_RATIO = 0.2
+BATCH_SIZE = 4
+IMG_SIZE = (128,128)
+SHUFFLE = True    
+NUM_CLASSES = 3
+RANDOM_SEED = 120
+AUG = {
+    'FLIP_H': 0.3,
+    'FLIP_V': 0.6,
+    'ROTATE_CW': 0.4,
+    'ROTATE_CCW': 0.7,
+    'TRANSLATE': [5, 0.3],
+    'RAND_BRIGHTNESS': 0.5,
+}
+
+class DataLoader:
+    def __init__(self, 
+                 path, 
+                 split_ratio=TRAIN_VAL_RATIO,
+                 batch_size = BATCH_SIZE,
+                 img_size = IMG_SIZE,
+                 shuffle = SHUFFLE,
+                 num_classes = NUM_CLASSES,
+                 random_seed = RANDOM_SEED,
+                 augment_conf = AUG):
+        
+        super(DataLoader, self).__init__()        
+        self.path = path
+        self.split_ratio = split_ratio
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.shuffle = shuffle
+        self.num_classes = num_classes
+        self.random_seed = random_seed
+        self.augment_conf = augment_conf
+
+        assert exists(self.path), "Dataset file does not exist, please insert the right input filepath."
+
+        print('Importing the datasets with the following parameters...')
+        print('   Dataset path                    :', self.path)
+        print('   Train-Val dataset ratio        :', self.split_ratio)
+
+        self.train_img_paths, self.val_img_paths, self.train_mask_paths, self.val_mask_paths = self.read_dataset_path(subset='TrainVal')
+        self.test_img_paths, self.test_mask_paths = self.read_dataset_path(subset='Test')
+
+        assert len(self.train_img_paths) == len(self.train_mask_paths), "image length files do not match with masks lenth in the training data"
+        assert len(self.val_img_paths) == len(self.val_mask_paths), "image length files do not match with masks lenth in the validation data"
+        assert len(self.test_img_paths) == len(self.test_mask_paths), "image length files do not match with masks lenth in the testing data"
+
+        print('num of training data: ', len(self.train_img_paths))
+        print('num of validation data: ', len(self.val_img_paths))
+        print('num of testing data: ', len(self.test_img_paths))
 
 
-RANDOM_SEED = 100
-BATCH_SIZE = 2
-TRAIN_VAL_RATIO = 0.7
-random.seed(RANDOM_SEED)
+    def read_dataset_path(self, subset='TrainVal'):
 
-    # def __init__(self, path, train_val_ratio=TRAIN_VAL_RATIO, batch_size=BATCH_SIZE):
-    #     super(DatasetLoader, self).__init__()
-    #     self.path = path
-    #     self.train_val_ratio = train_val_ratio
-    #     self.batch_size = batch_size
-
-def read_dataset(path):
-
-    assert exists(path), "Dataset file does not exist, please insert the right input filepath."
-
-    print('Importing the datasets with the following parameters...')
-    print('   Dataset path                    :', path)
-    print('   Train-Val dataset ratio        :', TRAIN_VAL_RATIO)
-
-    trainval_img_paths = np.array(glob(os.path.join(path, "TrainVal/color/*.jpg")))
-    trainval_img_paths = sorted(trainval_img_paths)
-    trainval_label_path = np.array(glob(os.path.join(path, "TrainVal/label/*.png")))
-    trainval_label_path = sorted(trainval_label_path)
-
-    test_img_paths = np.array(glob(os.path.join(path, "Test/color/*.jpg")))
-    test_img_paths = sorted(test_img_paths)
-    test_label_paths = np.array(glob(os.path.join(path, "Test/label/*.png")))
-    test_label_paths = sorted(test_label_paths)
-
-    num_trainval_files = len(trainval_img_paths)
-    num_test_files = len(test_img_paths)
-
-    assert num_trainval_files == len(trainval_label_path), "number of trainval img is not same as its labels"
-    assert num_test_files == len(test_label_paths), "number of test img is not same as its labels"
-
-    print('\nSplitting training and validation sets...')
-
-    train_img_paths, val_img_paths, train_label_paths, val_label_paths = train_test_split(
-        trainval_img_paths, trainval_label_path, test_size=(1-TRAIN_VAL_RATIO), random_state=RANDOM_SEED)
-    
-    # print('HUFTTT', type(self.train_img_paths))
-    print('num of training data: ', len(train_img_paths))
-    print('num of validation data: ', len(val_img_paths))
-    print('num of testing data: ', num_test_files)
-
-    train_ds = tf.data.Dataset.from_tensor_slices((train_img_paths, train_label_paths))
-    val_ds = tf.data.Dataset.from_tensor_slices((val_img_paths, val_label_paths))
-    test_ds = tf.data.Dataset.from_tensor_slices((test_img_paths, test_label_paths))
-
-    return train_ds, val_ds, test_ds        
-
-
-def prepare_dataset(train_ds, val_ds, test_ds, flip=None, rotate=None, translate=None):
-    # def augment_wrapper(img, label):
-    #     flipped, rot, trans = img_augment(img, label, flip=flip, rotate=rotate, translate=translate)
-    #     augmented_ds = tf.data.Dataset.from_tensors((img, label))
-    #     augmented_ds = augmented_ds.concatenate(tf.data.Dataset.from_tensors(flipped))
-    #     augmented_ds = augmented_ds.concatenate(tf.data.Dataset.from_tensors(rot))
-    #     augmented_ds = augmented_ds.concatenate(tf.data.Dataset.from_tensors(trans))
-    #     return augmented_ds
-    
-    # train_ds, val_ds, test_ds = read_dataset()
-    train_ds = train_ds.map(lambda img, label: img_augment(img, label, flip=flip, rotate=rotate, translate=translate),
-                        num_parallel_calls=tf.data.AUTOTUNE)
-    train_ds = train_ds.cache().shuffle(50).repeat(3).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-
-    val_ds = val_ds.cache().shuffle(50).map(img_preprocessing, num_parallel_calls=tf.data.AUTOTUNE).repeat(3).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-
-    test_ds = test_ds.map(img_preprocessing, num_parallel_calls=tf.data.AUTOTUNE).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
-    train_ds = iter(train_ds)
-    val_ds = iter(val_ds)
-    test_ds = iter(test_ds)
-
-    return train_ds, val_ds, test_ds
-    
-    # def __len__(self):
-    #     return len(self.train_img_paths)*4 // self.batch_size
-    
-    # def __getitem__(self, index):
-    #     return self.data[index]
-
-
-def img_preprocessing(img_path, label_path, shape=(128,128)):
-
-    # img = cv2.imread(str(img_path))
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # img = np.array(img, dtype=np.float32)/255.0
-
-    # label = cv2.imread(label_path)
-    # label = cv2.cvtColor(label, cv2.COLOR_BGR2RGB)
-
-    # is_black = np.all(label == [0,0,0], axis=-1)
-    # is_white = np.all(label == [255,255,255], axis=-1)
-    # is_red = (label[...,0] == 128)
-    # is_green = (label[...,2] == 128)
-
-    # if is_red.any():
-    #     is_red |= is_white
-    # else:
-    #     is_green |= is_white
-
-    # ch_1 = is_black.astype(np.uint8)
-    # ch_2 = is_red.astype(np.uint8)
-    # ch_3 = is_green.astype(np.uint8)
-
-    # label = np.stack([ch_1, ch_2, ch_3], axis=-1)
-
-    img = tf.io.read_file(img_path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.image.resize(img, shape)
-    img = img /255.0
-    img = tf.cast(img, dtype=tf.float32)
-
-    label = tf.io.read_file(label_path)
-    label = tf.io.decode_png(label, channels=3)
-    label = tf.image.resize(label, shape, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    
-    is_black = tf.reduce_all(tf.equal(label, [0, 0, 0]), axis=-1)
-    is_white = tf.reduce_all(tf.equal(label, [255, 255, 255]), axis=-1)
-    is_red = tf.equal(label[..., 0], 128)
-    is_green = tf.equal(label[..., 1], 128)
-    
-    red_count = tf.reduce_sum(tf.cast(is_red, tf.int32))
-    green_count = tf.reduce_sum(tf.cast(is_green, tf.int32))
-
-    # Apply the logic
-    def turn_white_red():
-        return tf.logical_or(is_red, is_white), is_green
-
-    def turn_white_green():
-        return is_red, tf.logical_or(is_green, is_white)
-
-    new_is_red, new_is_green = tf.cond(red_count > green_count, turn_white_red, turn_white_green)
-
-    ch_1 = tf.cast(is_black, tf.uint8)
-    ch_2 = tf.cast(new_is_red, tf.uint8)
-    ch_3 = tf.cast(new_is_green, tf.uint8)
-
-    
-    label = tf.stack([ch_1, ch_2, ch_3], axis=-1)
-
-
-    return img, label
-
-def img_augment(img, label, flip=None, rotate=None, translate=None):
-    def _image_flip(img, label, mode):
-
-        if mode == 'horizontal':
-            img = tf.image.flip_left_right(img)
-            label = tf.image.flip_left_right(label)
-        elif mode == 'vertical':
-            img = tf.image.flip_up_down(img)
-            label = tf.image.flip_up_down(label)
+        image_dir = os.path.join(self.path, subset, 'color')
+        mask_dir = os.path.join(self.path, subset, 'label')
+        
+        image_paths = sorted([os.path.join(image_dir, fname) for fname in os.listdir(image_dir) if fname.endswith('.jpg')])
+        mask_paths = sorted([os.path.join(mask_dir, fname) for fname in os.listdir(mask_dir) if fname.endswith('.png')])
+        
+        if subset == 'TrainVal':
+            print('\nSplitting training and validation sets...')
+            train_img_paths, val_img_paths, train_mask_paths, val_mask_paths = train_test_split(
+                image_paths, mask_paths, test_size=self.split_ratio, random_state=self.random_seed)
+            return train_img_paths, val_img_paths, train_mask_paths, val_mask_paths
+        elif subset == 'Test':
+            return image_paths, mask_paths
         else:
-            print(f'mode is not recognized: {mode}. Rederence: horizontal or vertical. No flipping algorithm is performed')
+            raise KeyError ("subset is wrong")
 
-        return img, label
+    def create_dataset(self):
+        def load_image_mask(image_path, mask_path, image_size=(128, 128), num_classes=3):
 
-    def _image_rotate(img, label, mode):
+            image = tf.io.read_file(image_path)
+            image = tf.image.decode_jpeg(image, channels=3)
+            image = tf.image.resize(image, image_size) / 255.0  
+            
+            mask = tf.io.read_file(mask_path)
+            mask = tf.image.decode_png(mask, channels=3)
+            mask = tf.image.resize(mask, image_size, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-        if mode == 'ccw':
-            img = tf.image.rot90(img, k=1)
-            label = tf.image.rot90(label, k=1)
-        elif mode == 'cw':
-            img = tf.image.rot90(img, k=3)
-            label = tf.image.rot90(label, k=3)
-        else:
-            print(f'mode is not recognized: {mode}. Reference: cw or ccw. No rotate algorithm is performed')
+            is_black = tf.reduce_all(tf.equal(mask, [0, 0, 0]), axis=-1)
+            is_white = tf.reduce_all(tf.equal(mask, [255, 255, 255]), axis=-1)
+            is_red = tf.equal(mask[..., 0], 128)
+            is_green = tf.equal(mask[..., 1], 128)
 
-        return img, label
+            red_count = tf.reduce_sum(tf.cast(is_red, tf.uint8))
+            green_count = tf.reduce_sum(tf.cast(is_green, tf.uint8))
+
+            def turn_white_red():
+                return tf.logical_or(is_red, is_white), is_green
+
+            def turn_white_green():
+                return is_red, tf.logical_or(is_green, is_white)
+
+            new_is_red, new_is_green = tf.cond(red_count > green_count, turn_white_red, turn_white_green)
+
+            ch_1 = tf.cast(is_black, tf.uint8) * 0
+            ch_2 = tf.cast(new_is_red, tf.uint8) * 1
+            ch_3 = tf.cast(new_is_green, tf.uint8) * 2
+            label = ch_1 + ch_2 + ch_3
+            label = tf.reshape(label, (128,128,1))
+            return image, label
+
+        def augment(image, mask):
+            
+            if tf.random.uniform(()) <= self.augment_conf['FLIP_H']:
+                image = tf.image.flip_left_right(image)
+                mask = tf.image.flip_left_right(mask)
+            
+            if tf.random.uniform(()) <= self.augment_conf['FLIP_V']:
+                image = tf.image.flip_up_down(image)
+                mask = tf.image.flip_up_down(mask)
+
+            if tf.random.uniform(()) <= self.augment_conf['ROTATE_CW']:
+                image = tf.image.rot90(image)
+                mask = tf.image.rot90(mask)
+                rot_ccw = False
+            else:
+                rot_ccw = True
+            
+            if tf.random.uniform(()) <= self.augment_conf['ROTATE_CCW'] and rot_ccw == True:
+                image = tf.image.rot90(image, k=3)
+                mask = tf.image.rot90(mask, k=3)
+            
+            if tf.random.uniform(()) <= self.augment_conf['TRANSLATE'][1]:
+                dx = tf.random.uniform([], -1*self.augment_conf['TRANSLATE'][0], 
+                                       self.augment_conf['TRANSLATE'][0], dtype=tf.int32)
+                dy = tf.random.uniform([], -1*self.augment_conf['TRANSLATE'][0], 
+                                       self.augment_conf['TRANSLATE'][0], dtype=tf.int32)
+                image = tf.roll(image, shift=[dx, dy], axis=[0, 1])
+                mask = tf.roll(mask, shift=[dx, dy], axis=[0, 1])
+                
+            if tf.random.uniform(()) <= self.augment_conf['RAND_BRIGHTNESS']:      
+                image = tf.image.random_brightness(image, max_delta=0.2)
+            
+            return image, mask
     
-    def _image_translate(img, label, mode):
+        def build_dataset(image_paths, mask_paths, augment_data=True):
+            dataset = tf.data.Dataset.from_tensor_slices((image_paths, mask_paths))
+            dataset = dataset.map(lambda img, mask: load_image_mask(img, mask, self.img_size, self.num_classes), num_parallel_calls=tf.data.AUTOTUNE)
+            if augment_data:
+                dataset = dataset.map(augment, num_parallel_calls=tf.data.AUTOTUNE)
+            dataset = dataset.cache().shuffle(1000).batch(self.batch_size).prefetch(tf.data.AUTOTUNE)
+            return dataset
+        
+        train_dataset = build_dataset(self.train_img_paths, self.train_mask_paths, augment_data=True)
+        val_dataset = build_dataset(self.val_img_paths, self.val_mask_paths, augment_data=False)
+        test_dataset = build_dataset(self.test_img_paths, self.test_img_paths, augment_data=False)
+        
+        return train_dataset, val_dataset, test_dataset
 
-        if mode:
-            dx = tf.random.uniform([], -5, 5, dtype=tf.int32)
-            dy = tf.random.uniform([], -5, 5, dtype=tf.int32)
-            img = tf.roll(img, shift=[dx, dy], axis=[0, 1])
-            label = tf.roll(label, shift=[dx, dy], axis=[0, 1])
-        else:
-            print(f'mode is not recognized: {mode}. Reference: Yes. No translation algorithm is performed')
-
-        return img, label
-    
-    # flip_img, flip_label = _image_flip(img, label, flip)
-    # rot_img, rot_label = _image_rotate(img, label, rotate)
-    # trans_img, trans_label = _image_translate(img, label, translate)
-    # return (flip_img, flip_label), (rot_img, rot_label), (trans_img, trans_label)
-
-    img, label = tf.py_function(img_preprocessing, [img, label], Tout=[tf.float32, tf.uint8])
-
-    if tf.random.uniform(()) > 0.5:
-        img, label = _image_flip(img, label, flip)
-    if tf.random.uniform(()) > 0.5:
-        img, label = _image_rotate(img, label, rotate)
-    if tf.random.uniform(()) > 0.5:
-        img, label = _image_translate(img, label, translate)
-    return img, label
-    
 if __name__ == "__main__":
-    train_ds, val_ds, test_ds = read_dataset('./Dataset/Dataset')
-    a, b, c = prepare_dataset(train_ds, val_ds, test_ds, flip='horizontal',
-                                                          rotate='ccw',
-                                                          translate=30)
+    dataset_path = "Dataset"
+    data_loader = DataLoader(dataset_path)
+    train_dataset, val_dataset, test_dataset = data_loader.create_dataset()
